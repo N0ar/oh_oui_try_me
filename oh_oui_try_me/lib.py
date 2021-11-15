6c7e4b72ef6efa75c0e70a41f537f1a392315233
Arnaud Vuacheux <avuacheux@gmail.com>
2021-11-15 15:39:47 +0100

    last version oh-oui-try-me

--- oh_oui_try_me/lib.py
+++ oh_oui_try_me/lib.py
@@ -1,12 +1,25 @@
 import random
 
+
 def try_me():
     acronyme = [
-        'ATM', 'BBBJ', 'BBFS', 'BBJTCNQNS', 'BLS', 'CIF', 'DATO', 'DP', 'FBSM',
-        'MSOG', 'OWO', 'RPG', 'SOMF',
+        'ATM',
+        'BBBJ',
+        'BBFS',
+        'BBJTCNQNS',
+        'BLS',
+        'CIF',
+        'DATO',
+        'DP',
+        'FBSM',
+        'MSOG',
+        'OWO',
+        'RPG',
+        'SOMF',
     ]
-    i = random.randint(0, len(acronyme))
-    return acronyme[i]
+    i = random.randint(0, len(acronyme)-1)
+    print(acronyme[i])
+
 
 if __name__ == "__main__":
-    print(try_me())
+    try_me()
